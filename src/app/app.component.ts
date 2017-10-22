273c7d2b97caa03473d75a1a9069014e2c92f71c
Ihor <iostapovychweb@gmail.com>
2017-10-22 12:41:37 +0300

    Event-based navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private showRecipe = true;
+
+  changeView(event: string) {
+    this.showRecipe = event === 'recipes' ? true : false;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() public showComponent = new EventEmitter<string>();
 
+  public onSelect(view): void {
+    this.showComponent.emit(view);
+  }
 }
